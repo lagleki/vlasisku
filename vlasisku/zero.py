19ae413a1adacd673abdbbbdb0eed5c7ab404023
Alex Burka <alex@alexburka.com>
2014-08-09 04:38:18 +0000

    rpc parser client

---
+++ vlasisku/zero.py
@@ -0,0 +1,13 @@
+import zerorpc
+
+# this only happens once, since modules are singletons
+client = zerorpc.Client()
+client.connect("tcp://127.0.0.1:4242")
+
+def parse(text, rule="text", mode=1):
+    result = client.parse(text, rule, mode)
+    if isinstance(result, dict):
+        raise Exception("%(name)s from camxes: %(message)s (line %(line)d, column %(column)d, offset %(offset)d)" % result)
+    else:
+        return result
+
