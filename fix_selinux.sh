d9ca2f20161913d83fdcfcce3100e4d6e3bd6452
Lojban Webmaster <webmaster@lojban.org>
2019-01-27 19:11:45 -0800

    Handle manage.py

--- fix_selinux.sh
+++ fix_selinux.sh
@@ -2,7 +2,8 @@
 
 chcon -R -t systemd_unit_file_t systemd/
 chcon -t home_bin_t build_docker.sh fix_selinux.sh kill_docker.sh run_docker.sh setup.sh cron_reload_db.sh run_bots_docker.sh
-loginctl show-user sampre_vs | grep Linger=yes || (
+chcon -t container_file_t manage.py
+loginctl show-user sampre_vs | grep -q Linger=yes || (
 	echo -e "\n\n\nUSER LINGER DISABLED FOR THIS USER.  VERY BAD; MUST FIX.\n\n\n" ;
 	loginctl enable-linger sampre_vs
 )
