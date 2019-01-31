a2b95dd7dbfbba0e3894815740dd19ba00da31f6
Lojban Webmaster <webmaster@lojban.org>
2019-01-30 17:31:48 -0800

    Better handling of bot stopping under podman.

---
+++ stop_bots_container.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+exec 2>&1
+set -e
+set -x
+
+CONTAINER_BIN=${CONTAINER_BIN:-$(which podman)}
+CONTAINER_BIN=${CONTAINER_BIN:-$(which docker)}
+
+test=""
+# Test mode
+if [ "$1" = "-t" ]
+then
+	test="_test"
+fi
+
+# Ask for a tty if that makes sense
+hasterm=''
+if tty -s
+then
+	hasterm='-t'
+fi
+
+echo
+echo "Setting up config files and the like."
+echo
+
+cd /srv/lojban/vlasisku
+./fix_selinux.sh
+
+echo
+echo "Killing container bots inside the previous container instance"
+echo
+
+while sudo $CONTAINER_BIN exec -i $hasterm lojban_vlasisku${test} pgrep -f runbots
+do
+	sudo $CONTAINER_BIN exec -i $hasterm lojban_vlasisku${test} pkill -f runbots || true
+	sleep 1
+	sudo $CONTAINER_BIN exec -i $hasterm lojban_vlasisku${test} pkill -9 -f runbots || true
+done
+
+sudo $CONTAINER_BIN exec -i $hasterm lojban_vlasisku${test} pgrep -f runbots || exit 0
